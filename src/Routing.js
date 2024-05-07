7b182b96d30b3f3c55a369e6d322a7b3492c90bd
joshuasparkes <joshsparkes6@gmail.com>
2024-05-07 11:32:56 +0100

    tweaks

--- src/Routing.js
+++ src/Routing.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Search from "./search.js";
-import OperatorSearch from "./operatorSearch.js";
 
 function Routing() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/operatorSearch" element={<OperatorSearch />} />
         <Route path="/" element={<Search />} />
         <Route path="/search" element={<Search />} />
       </Routes>
