7cf16d52b12096f7c2c9b8ddcbba701acadbd1cc
joshuasparkes <joshsparkes6@gmail.com>
2024-07-04 15:12:49 +0100

    UI updates

--- src/ListBookings.js
+++ src/ListBookings.js
@@ -2,8 +2,20 @@ import React from "react";
 
 function ListBookings() {
   return (
-    // List Area
     <div>
+      <div>
+        {/* Input fields go in this container */}
+        <input />
+        <input />
+        <button>Search</button>
+      </div>
+      {/* Response items go in this container */}
+    <ul>
+        <li className="list-item"></li>
+        <li className="list-item"></li>
+        <li className="list-item"></li>
+        <li className="list-item"></li>
+      </ul>
     </div>
   );
 }
