398d678e6ad66cf0b0918127d524f84af0b1e887
joshuasparkes <joshsparkes6@gmail.com>
2024-07-12 13:32:53 +0100

    added driver locaiton

---
+++ src/DriverLocations.js
@@ -0,0 +1,38 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const DriverLocations = () => {
+  return (
+    <div>
+      <div>
+        <p className="driver-locations">Driver Locations</p>
+        <div className="location-search">
+          {/* Input fields go in this container */}
+          <input />
+          <input />
+          <button>Search</button>
+        </div>
+      </div>
+      {/* Response items go in this container */}
+      <ul>
+        <li className="location-item">Result 1</li>
+        <li className="location-item">Result 2</li>
+      </ul>
+      <MapContainer
+        center={[52, 0]}
+        zoom={2}
+        style={{ height: "400px", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={[52, 0]}>
+          <Popup></Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default DriverLocations;
--- src/RideHailing.js
+++ src/RideHailing.js
@@ -1,5 +1,4 @@
-// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import DriverLocations from "./DriverLocations";
 
 const RideHailing = () => {
   return (
@@ -21,22 +20,7 @@ const RideHailing = () => {
         <li className="offer-item">Result 4</li>
         <li className="offer-item">Result 5</li>
       </ul>
-      {/* <MapContainer
-        center={[52, 0]}
-        zoom={2}
-        style={{ height: "400px", width: "100%" }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker
-          key={}
-          position={[]}
-        >
-          <Popup></Popup>
-        </Marker>
-      </MapContainer> */}
+      <DriverLocations/>
     </div>
   );
 };
