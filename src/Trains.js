97ccca800afef2bfd968148a914156da3b26fd39
joshuasparkes <joshsparkes6@gmail.com>
2024-08-19 15:53:08 +0100

    test

--- src/Trains.js
+++ src/Trains.js
@@ -85,7 +85,7 @@ const Trains = () => {
 
       setResults(extractedPrices);
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error("Error fetching datas:", error);
     } finally {
       setLoading(false);
     }
