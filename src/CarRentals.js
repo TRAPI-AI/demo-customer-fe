254384169f44cdfafeca74ab203f3d301833a8e9
joshuasparkes <joshsparkes6@gmail.com>
2024-11-04 13:16:04 +0000

    updates

--- src/CarRentals.js
+++ src/CarRentals.js
@@ -4,27 +4,26 @@ const CarRentals = () => {
       <div className="search-area">
         <div className="search">
           {/* Input fields go in this container */}
-          <input className="origin" placeholder='Origin' />
+          <input className="origin" placeholder="Origin" />
           <input className="destination" />
-          <input className="from" />
-          <input className="to" />
+          <input className="date-from" />
+          <input className="date-to" />
           <button>Search</button>
         </div>
       </div>
       {/* Response items go in this container */}
-     <ul>
-            <li
-              className="search-response-item"
-            >
-              <p className="status">status</p>
-              <p className="capacity">capacity</p>
-              <p className="beds">beds</p>
-              <p className="price-per-day">price-per-day</p>
-              <p className="total-price">
-                total price<span className="currency">currency</span>
-              </p>
-            </li>
-        </ul>
+      <ul>
+        <li className="search-response-item">
+          <p className="status">status</p>
+          <p className="capacity">capacity</p>
+          <p className="beds">beds</p>
+          <p className="price-per-day">price-per-day</p>
+          <p className="total-price">
+            total price<span className="currency">currency</span>
+          </p>
+          <button className="select-button">Select</button>
+        </li>
+      </ul>
     </div>
   );
 };
