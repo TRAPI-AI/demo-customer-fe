a35aa985771074328c90c928844ffef469fa3424
joshuasparkes <joshsparkes6@gmail.com>
2024-11-04 13:32:18 +0000

    add image

--- src/CarRentals.js
+++ src/CarRentals.js
@@ -6,20 +6,26 @@ const CarRentals = () => {
           {/* Input fields go in this container */}
           <input className="origin" placeholder="Origin" />
           <input className="destination" placeholder="Destination" />
-          <input className="date-from" type="date"/>
-          <input className="date-to" type="date"/>
+          <input className="date-from" type="date" />
+          <input className="date-to" type="date" />
           <button>Search</button>
         </div>
       </div>
       <ul>
         <li className="search-response-item">
-          <p className="status">status</p>
-          <p className="capacity">capacity</p>
-          <p className="beds">beds</p>
-          <p className="price-per-day">price-per-day</p>
-          <p className="total-price">
-            total price<span className="currency">currency</span>
-          </p>
+          <img
+            alt="Placeholder Image"
+            className="vehicle-image"
+          />
+          <div>
+            <p className="status">status</p>
+            <p className="capacity">capacity</p>
+            <p className="beds">beds</p>
+            <p className="price-per-day">price-per-day</p>
+            <p className="total-price">
+              total price<span className="currency">currency</span>
+            </p>
+          </div>
           <button className="select-button">Select</button>
         </li>
       </ul>
